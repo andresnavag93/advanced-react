026ef17348080cc3dd6788cc70e850f73704a2af
anavarrogamaga <anavarro@gamaga.com>
2023-03-15 18:38:50 +0100

    :hammer: Step 2 Landing Section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -15,6 +15,12 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack spacing={4}>
+      <Avatar name="Pete" src="https://i.pravatar.cc/150?img=7" size="2xl" />
+      <Heading size="xl" color="white">{greeting}</Heading>
+      <Heading size="md" color="white">{bio1}</Heading>
+      <Heading size="md" color="white">{bio2}</Heading>
+    </VStack>
 
   </FullScreenSection>
 );
